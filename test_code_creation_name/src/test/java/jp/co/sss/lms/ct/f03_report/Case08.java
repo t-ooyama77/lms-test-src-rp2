a4d859dba2b13ae9c5e57d3409efd39f5b6394cc
t-ooyama <t_ooyama@braingate.co.jp>
2025-10-24 12:01:04 +0900

    fix:Case08.java 追記 add:Case08_test01.png 新規作成 add:Case08_test02.png 新規作成 add:Case08_test03.png 新規作成 add:Case08_test04.png 新規作成 add:Case08_test05.png 新規作成 add:Case08_test06.png 新規作成 add:Case08_test07.png 新規作成 fix:RP2_10_結合テスト仕様書_1.1.xls

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,49 +40,147 @@ public class Case08 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページに遷移
+		goTo("http://localhost:8080/lms");
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement login = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", login.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログインIDを入力する
+		WebElement loginId = webDriver.findElement(By.name("loginId"));
+		loginId.clear(); // 既存の値を消す（任意）
+		loginId.sendKeys("StudentAA02"); //初回ログイン済みのログインID
+
+		//ログインIDを入力する
+		WebElement password = webDriver.findElement(By.name("password"));
+		password.clear(); // 既存の値を消す（任意）
+		password.sendKeys("studentA02"); //初回ログイン済みのpassword
+
+		//ログインボタンを押下
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		loginButton.click();
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement loginSuccess = webDriver.findElement(By.tagName("small"));
+		assertEquals("ようこそ受講生ＡＡ２さん", loginSuccess.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 提出済の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//週報が提出済みのセクションの「詳細」ボタンを押下する(上から2番目の「詳細」ボタンを押す)
+		List<WebElement> detailButtons = webDriver.findElements(By.xpath("//input[@value='詳細']"));
+		if (detailButtons.size() > 1) {
+			detailButtons.get(1).click();
+		}
+
+		//セクション詳細画面に遷移しているかを確認
+		WebElement section = webDriver.findElement(By.linkText("コース詳細"));//コース詳細画面へのリンク
+		assertEquals("コース詳細", section.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「確認する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//クリックできるように画面をスクロールする
+		scrollTo("500");
+		//「提出済み週報【デモ】を確認する」ボタンを押下してレポート登録画面に遷移する
+		WebElement submit = webDriver.findElement(By.xpath("//input[@value='提出済み週報【デモ】を確認する']"));
+		submit.click();
+
+		//レポート登録画面に遷移しているかを確認
+		WebElement report = webDriver.findElement(By.tagName("h2"));
+		assertEquals("週報【デモ】 2025年7月9日", report.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しセクション詳細画面に遷移")
 	void test05() {
-		// TODO ここに追加
+		//入力できるように画面をスクロールする
+		scrollTo("500");
+		//週報の「一週間の振り返り」欄の入力内容をを修正する
+		WebElement reportInput = webDriver.findElement(By.name("contentArray[2]"));
+		reportInput.clear();
+		reportInput.sendKeys("毎日予習復習を行ってがんばります！");
+
+		//修正が反映されているかを確認する
+		WebElement reportContent = webDriver.findElement(By.name("contentArray[2]"));
+		assertEquals("毎日予習復習を行ってがんばります！", reportContent.getText());
+
+		//「提出する」ボタンを押下してセクション詳細画面に遷移する
+		WebElement reportSubmit = webDriver.findElement(By.className("btn-primary"));
+		reportSubmit.click();
+
+		//セクション詳細画面に遷移したことを確認する
+		WebElement section = webDriver.findElement(By.tagName("h2"));
+		assertEquals("アルゴリズム、フローチャート 2025年7月9日", section.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test06() {
-		// TODO ここに追加
+		//ユーザー名のリンクからユーザー詳細画面に遷移する
+		WebElement userName = webDriver.findElement(By.xpath("//a[small[text()='ようこそ受講生ＡＡ２さん']]"));
+		userName.click();
+
+		//ユーザー詳細画面に遷移しているかを確認する
+		WebElement userDetail = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ユーザー詳細", userDetail.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映される")
 	void test07() {
-		// TODO ここに追加
+		//クリックできるように画面をスクロールする
+		scrollTo("1000");
+		//週報の「詳細」ボタンを押下する(上から4番目の「詳細」ボタンを押す)
+		List<WebElement> detailButtons = webDriver.findElements(By.xpath("//input[@value='詳細']"));
+		detailButtons.get(3).click();
+
+		//表示された週報の内容が修正した内容になっているかを確認する
+		List<WebElement> tables = webDriver.findElements(By.tagName("table"));
+		List<WebElement> tds = tables.get(2).findElements(By.tagName("td"));
+		WebElement weeklyReview = tds.get(2);
+		assertEquals("毎日予習復習を行ってがんばります！", weeklyReview.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 }
