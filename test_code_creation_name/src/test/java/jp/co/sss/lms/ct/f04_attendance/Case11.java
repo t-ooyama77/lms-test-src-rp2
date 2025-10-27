ee6b5b1e196785518866fb78f42f50dc018e4130
t-ooyama <t_ooyama@braingate.co.jp>
2025-10-28 00:47:28 +0900

    fix:Case09.java 修正 fix:Case11.java 追記 add:Case11_test01.png 新規作成 add:Case11_test02.png 新規作成 add:Case11_test03.png 新規作成 add:Case11_test04.png 新規作成 add:Case11_test05.png 新規作成 add:ConstantsMessages.java 新規作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
@@ -3,6 +3,12 @@ package jp.co.sss.lms.ct.f04_attendance;
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -10,8 +16,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -85,6 +94,14 @@ public class Case11 {
 		WebElement attendance = webDriver.findElement(By.linkText("勤怠"));
 		attendance.click();
 
+		// アラートが出ているか確認して処理
+		try {
+			Alert alert = webDriver.switchTo().alert();
+			alert.accept(); // OKボタンを押す
+		} catch (NoAlertPresentException e) {
+			// アラートがなければ何もしない
+		}
+
 		//正しい表示画面に遷移しているかを確認
 		WebElement attendanceManagement = webDriver.findElement(By.tagName("h2"));
 		assertEquals("勤怠管理", attendanceManagement.getText());
@@ -114,8 +131,93 @@ public class Case11 {
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 すべての研修日程の勤怠情報を正しく更新し勤怠管理画面に遷移")
-	void test05() {
-		// TODO ここに追加
+	void test05() throws InterruptedException {
+		//テーブルのtdody要素を取得する
+		WebElement tbody = webDriver.findElement(By.tagName("tbody"));
+		//trタグを取得する
+		List<WebElement> trTags = tbody.findElements(By.tagName("tr"));
+
+		// yyyy年MM月dd日 の部分だけを抜き出す正規表現
+		Pattern datePattern = Pattern.compile("(\\d{4}年\\d{2}月\\d{2}日)");
+
+		//日付の比較の準備
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日");
+		LocalDate today = LocalDate.now();//今日の日付
+
+		for (WebElement tr : trTags) {
+			List<WebElement> tdTags = tr.findElements(By.tagName("td"));
+			Matcher matcher = datePattern.matcher(tdTags.get(0).getText());
+			if (matcher.find()) {
+				String dateString = matcher.group(1);
+				System.out.println(dateString);
+
+				// 文字列をLocalDateに変換
+				LocalDate date = LocalDate.parse(dateString, formatter);
+
+				// 日付を比較して本日とそれ以前の日付には「定時」ボタンをクリックする
+				if (date.isBefore(today) || date.equals(today)) {
+					WebElement button = tdTags.get(2).findElement(By.tagName("button"));
+					//画面を少し下にずらしてクリック
+					scrollBy("50");
+					button.click();
+
+					//一秒待つ
+					Thread.sleep(1000);
+				}
+			}
+
+		}
+
+		/*最初の日付の出勤が9:00 退勤が18:00 であるかを確認する*/
+		//要素を取得
+		WebElement startHour = webDriver.findElement(By.xpath("//select[@id='startHour0']"));
+		WebElement startMinute = webDriver.findElement(By.xpath("//select[@id='startMinute0']"));
+		WebElement endHour = webDriver.findElement(By.xpath("//select[@id='endHour0']"));
+		WebElement endMinute = webDriver.findElement(By.xpath("//select[@id='endMinute0']"));
+
+		// Selectクラスを使って操作
+		Select selectStartHour = new Select(startHour);
+		Select selectStartMinute = new Select(startMinute);
+		Select selectEndHour = new Select(endHour);
+		Select selectEndMinute = new Select(endMinute);
+
+		// 現在選択されているオプションのテキストを取得
+		String startHourStr = selectStartHour.getFirstSelectedOption().getText();
+		String startMinuteStr = selectStartMinute.getFirstSelectedOption().getText();
+		String endHourStr = selectEndHour.getFirstSelectedOption().getText();
+		String endMinuteStr = selectEndMinute.getFirstSelectedOption().getText();
+
+		assertEquals("09", startHourStr);
+		assertEquals("00", startMinuteStr);
+		assertEquals("18", endHourStr);
+		assertEquals("00", endMinuteStr);
+
+		//クリックできるように画面をスクロールする
+		scrollTo("1000");
+
+		//「更新」ボタンを押下して勤怠管理画面に遷移する
+		WebElement update = webDriver.findElement(By.name("complete"));
+		update.click();
+
+		//一秒待つ
+		Thread.sleep(1000);
+
+		// アラートが出ているか確認して処理
+		try {
+			Alert alert = webDriver.switchTo().alert();
+			alert.accept(); // OKボタンを押す
+		} catch (NoAlertPresentException e) {
+			// アラートがなければ何もしない
+		}
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement attendanceManagement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", attendanceManagement.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
---
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/util/ConstantsMessages.java
@@ -0,0 +1,8 @@
+package jp.co.sss.lms.ct.util;
+
+public class ConstantsMessages {
+
+	public static final String len500 = "あ".repeat(500);
+	public static final String len501 = "あ".repeat(500) + "い";
+
+}
