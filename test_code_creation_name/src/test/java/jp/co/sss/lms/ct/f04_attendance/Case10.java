e0f97a6e1a45bebfd33c5b08f0cb9c915e94ff80
t-ooyama <t_ooyama@braingate.co.jp>
2025-10-27 17:11:21 +0900

    fix:Case10.java 追記 add:Case10_test01.png 新規作成 add:Case10_test02.png 新規作成 add:Case10_test03.png 新規作成 add:Case10_test04.png 新規作成 add:Case10_test05.png 新規作成 fix:RP2_結合テスト仕様書_1.1.xls 追記

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +39,102 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページに遷移
+		goTo("http://localhost:8080/lms");
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement login = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", login.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログインIDを入力する
+		WebElement loginId = webDriver.findElement(By.name("loginId"));
+		loginId.clear(); // 既存の値を消す（任意）
+		loginId.sendKeys("StudentAA02"); //初回ログイン済みのログインID
+
+		//ログインIDを入力する
+		WebElement password = webDriver.findElement(By.name("password"));
+		password.clear(); // 既存の値を消す（任意）
+		password.sendKeys("studentA02"); //初回ログイン済みのpassword
+
+		//ログインボタンを押下
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		loginButton.click();
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement loginSuccess = webDriver.findElement(By.tagName("small"));
+		assertEquals("ようこそ受講生ＡＡ２さん", loginSuccess.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//「勤怠」リンクをクリックする
+		WebElement attendance = webDriver.findElement(By.linkText("勤怠"));
+		attendance.click();
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement attendanceManagement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", attendanceManagement.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		//「出勤」ボタンを押下する
+		WebElement startWork = webDriver.findElement(By.xpath("//input[@value='出勤']"));
+		startWork.click();
+
+		//「打刻します。よろしいですか」のアラートが出るので処理する
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept(); // OKボタンを押す
+
+		//「勤怠情報の登録が完了しました。」のメッセージが出ているかを確認する
+		WebElement attendanceRegist = webDriver.findElement(By.xpath("(//div[contains(@class, 'alert')]//span)[2]"));
+		assertEquals("勤怠情報の登録が完了しました。", attendanceRegist.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+		//「退勤」ボタンを押下する
+		WebElement leaveWork = webDriver.findElement(By.xpath("//input[@value='退勤']"));
+		leaveWork.click();
+
+		//「打刻します。よろしいですか」のアラートが出るので処理する
+		Alert alert = webDriver.switchTo().alert();
+		alert.accept(); // OKボタンを押す
+
+		//「勤怠情報の登録が完了しました。」のメッセージが出ているかを確認する
+		WebElement attendanceRegist = webDriver.findElement(By.xpath("(//div[contains(@class, 'alert')]//span)[2]"));
+		assertEquals("勤怠情報の登録が完了しました。", attendanceRegist.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,28 +38,77 @@ public class Case11 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページに遷移
+		goTo("http://localhost:8080/lms");
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement login = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", login.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//ログインIDを入力する
+		WebElement loginId = webDriver.findElement(By.name("loginId"));
+		loginId.clear(); // 既存の値を消す（任意）
+		loginId.sendKeys("StudentAA02"); //初回ログイン済みのログインID
+
+		//ログインIDを入力する
+		WebElement password = webDriver.findElement(By.name("password"));
+		password.clear(); // 既存の値を消す（任意）
+		password.sendKeys("studentA02"); //初回ログイン済みのpassword
+
+		//ログインボタンを押下
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		loginButton.click();
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement loginSuccess = webDriver.findElement(By.tagName("small"));
+		assertEquals("ようこそ受講生ＡＡ２さん", loginSuccess.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//「勤怠」リンクをクリックする
+		WebElement attendance = webDriver.findElement(By.linkText("勤怠"));
+		attendance.click();
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement attendanceManagement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", attendanceManagement.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//「勤怠情報を直接編集する」リンクをクリックする
+		WebElement attendanceEdit = webDriver.findElement(By.linkText("勤怠情報を直接編集する"));
+		attendanceEdit.click();
+
+		//正しい表示画面に遷移しているかを確認
+		WebElement attendanceChange = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", attendanceChange.getText());
+
+		//画面をキャプチャして保存する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
